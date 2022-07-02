f447a99d6a9a37e78d89f66b8fa17eb86b0ddab1
Akshay Vhatkar <akshay0706vhatkar@gmail.com>
2022-07-02 20:46:49 +0530

    Day 2 completed

---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class home extends StatelessWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog App"),
+      ),
+      body: const Center(
+        child: Text("Hello World!"),
+      ),
+      drawer: const Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:catalog/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,12 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Material(
-            child: Center(
-      child: Container(
-        child: Text("Hello World!"),
-      ),
-    )));
+    return const MaterialApp(
+      home: home(),
+    );
   }
 }
